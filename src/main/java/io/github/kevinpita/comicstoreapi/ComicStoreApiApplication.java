d4066fc34237fa051945544a731af9316ea9000d
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-04-05 14:36:02 +0200

    feat: spring boot base created with spring initializr

---
+++ src/main/java/io/github/kevinpita/comicstoreapi/ComicStoreApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.kevinpita.comicstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicStoreApiApplication.class, args);
+	}
+
+}
